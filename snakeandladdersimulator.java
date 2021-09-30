7b7b32e0cd8012b7997634981378c70dd7edaa89
unknown <altamashbagwan1@gmail.com>
2021-09-30 16:02:33 +0530

    Added: Functioning of Playing Two Players Snake And Ladder

--- snakeandladdersimulator.java
+++ snakeandladdersimulator.java
@@ -2,41 +2,63 @@ package SnakeAndLadderSimulator;
 
 public class snakeandladdersimulator {
 	public static void main(String[] args) {
-		int playerPosition = playingLadderAndSnake();
-			System.out.println("The Player Position is: " + playerPosition);
+		playingLadderAndSnake();
 }
-			static int playingLadderAndSnake() {
-				// Constant
-				int LADDER = 1;
-				int SNAKE = 2;
-				// Variables
-				int startPlayer = 0;
-				int previousPosition;
-				int counter = 0;
-				while(true) {
-					int name = (int)(Math.random() * 3 );
-						System.out.println("the snake ladder "+name);
-					int diceNumber = (int)( (Math.random() * 6 ) + 1 );
-						System.out.println("the dice value is "+diceNumber);			
-					if ( name == LADDER ) {
-						previousPosition = startPlayer; 
-						startPlayer= startPlayer + diceNumber;
-						if ( startPlayer > 100 )
-							startPlayer = previousPosition;
+		static void playingLadderAndSnake() {
+			// Constant
+			int LADDER = 1;
+			int SNAKE = 2;
+			// Variables
+			int startPlayerOne = 0;
+			int previousPositionPlayerOne;
+			int startPlayerTwo = 0;
+			int previousPositionPlayerTwo;
+			while(true) {
+				int name = (int) (Math.random() * 3 );
+					System.out.println("the snake ladder " +name);
+				int diceNumber = (int)( (Math.random() * 6 ) + 1 );
+					System.out.println("the dice value is " +diceNumber);			
+				if ( name == LADDER ) {
+					previousPositionPlayerOne = startPlayerOne; 
+					startPlayerOne= startPlayerOne + diceNumber;
+					if ( startPlayerOne > 100 )
+						startPlayerOne = previousPositionPlayerOne;
+					continue;
+				}
+				else if ( name == SNAKE) {
+					startPlayerOne = startPlayerOne - diceNumber;
+					if ( startPlayerOne < 0 )
+						startPlayerOne = 0;
+				}
+				else 
+					startPlayerOne = startPlayerOne;
+					if ( startPlayerOne == 100 )	{
+						System.out.println("The PlayerOne Win First");
+						break;
 					}
-					else if ( name == SNAKE) {
-						startPlayer = startPlayer - diceNumber;
-						if ( startPlayer < 0 )
-							startPlayer = 0;
+				startingPlayerTwo: if (true) {
+				int naam = (int) (Math.random() * 3 );
+					System.out.println("the snake ladder " +naam);
+				int diceNumberTwo = (int)( (Math.random() * 6 ) + 1 );
+					System.out.println("the dice value is " +diceNumberTwo);			
+					if ( naam == LADDER ) {
+						previousPositionPlayerTwo = startPlayerTwo; 
+						startPlayerTwo= startPlayerTwo + diceNumberTwo;
+						if ( startPlayerTwo > 100 )
+							startPlayerTwo = previousPositionPlayerTwo;
+						}
+					else if ( naam == SNAKE) {
+						startPlayerTwo = startPlayerTwo - diceNumberTwo;
+						if ( startPlayerTwo < 0 )
+							startPlayerTwo = 0;
 					}
 					else 
-						startPlayer = startPlayer;
-						if ( startPlayer == 100 )
-							break;
-				counter++;
-				System.out.println("The Position of The Player: " +startPlayer);
+						startPlayerTwo = startPlayerTwo;
+					if ( startPlayerTwo == 100 )	{
+						System.out.println("The PlayerTwo Win First");
+					break;
+					}
 				}
-					System.out.println("The Total Number of dice counted: " +counter);
-					return startPlayer;
+			}
 		}
 }
