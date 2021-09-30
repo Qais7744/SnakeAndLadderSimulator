c5c0ac24ef26800cff4836c63ae73fb5c41198e0
unknown <altamashbagwan1@gmail.com>
2021-09-30 15:38:08 +0530

    Added: Functioning of Welcome Snake And Ladder Simulator

---
+++ snakeandladdersimulator.java
@@ -0,0 +1,7 @@
+package SnakeAndLadderSimulator;
+
+public class snakeandladdersimulator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake And Ladder Game ");
+	}
+}
